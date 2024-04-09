b758aedc03049fa1548defaf1500bf05fc5c278a
Aditya Kale <116991817+Adityakale12834@users.noreply.github.com>
2024-04-09 13:21:54 +0530

    Time: 3 ms (40.02%) | Memory: 41.1 MB (67.71%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int n = tickets.length;
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
+        while(tickets[k] != 0){
+            for(int i=0;i<n;i++){
+                if(tickets[i] != 0){
+                    tickets[i] = tickets[i] - 1;
+                    count++;
+                }
+                if(i == k && tickets[i] == 0){
+                    return count;
+                }
+            }
+        }
+        return tickets[n-1];
+    }
+}
