a8e042cb26e47b4cb82751d493d171d4dc48626e
Aditya Kale <116991817+Adityakale12834@users.noreply.github.com>
2024-05-03 21:08:02 +0530

    Time: 1 ms (90.68%) | Memory: 43.8 MB (5.05%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -2,22 +2,27 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         int m = s.length();
         int n = t.length();
-        return DFS(s,t,0,0);
+        Boolean[][] dp = new Boolean[m+1][n+1];
+        return DFS(s,t,0,0,dp);
     }
-    public boolean DFS(String s,String t,int i,int j){
+    public boolean DFS(String s,String t,int i,int j,Boolean[][] dp){
         if(i == s.length()){
             return true;
         }
         if(j == t.length()){
             return false;
         }
+        if(dp[i][j] != null){
+            return dp[i][j];
+        }
         boolean val = false;
         if(s.charAt(i) == t.charAt(j)){
-            val = DFS(s,t,i+1,j+1);
+            val = DFS(s,t,i+1,j+1,dp);
         }
         else{
-            val = DFS(s,t,i,j+1);
+            val = DFS(s,t,i,j+1,dp);
         }
+        dp[i][j] = val;
         return val;
     }
 }
